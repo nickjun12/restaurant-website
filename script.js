e856bf9d189737de3b368fde305208f6ab865556
nickjun12 <juniornicko6@gmail.com>
2024-08-13 06:19:53 +0700

    Restaurant Website

---
+++ script.js
@@ -0,0 +1,36 @@
+// STICKY NAVBAR
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 80);
+});
+
+// HAMBURGER MENU
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
+
+// SCROLL REVEAL
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "85px",
+  duration: 2500,
+  reset: true,
+});
+
+sr.reveal(".home-text", { delay: 300 });
+sr.reveal(".home-img", { delay: 400 });
+
+sr.reveal(".container", { delay: 400 });
+
+sr.reveal(".about-img", {});
+sr.reveal(".about-text,.btn", { delay: 300 });
+
+sr.reveal(".middle-text", {});
+sr.reveal(".shop-content,.row-btn", { delay: 300 });
+
+sr.reveal(".review-content", { delay: 300 });
